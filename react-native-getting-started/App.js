e1d9dde439ff2c15f2729484d55519b36161a1df
ShafiArnob <shafiarnob@gmail.com>
2023-10-27 00:31:57 +0600

    add and show list

--- react-native-getting-started/App.js
+++ react-native-getting-started/App.js
@@ -1,54 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  const [enteredGoalText, setenteredGoalText] = useState([])
+  const [enteredGoalText, setenteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
 
-  function goalInputHandler(enteredText){
-    setenteredGoalText(enteredText)
+  function goalInputHandler(enteredText) {
+    setenteredGoalText(enteredText);
   }
 
-  function addGoalHandler(){
-    console.log(enteredGoalText);
+  function addGoalHandler() {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
+    console.log(courseGoals);
   }
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer  }>
-        <TextInput style={styles.textInput} placeholder='Your Goal' onChangeText={goalInputHandler}/>
-        <Button title='Add Goal' onPress={addGoalHandler}/>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your Goal"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
 
       <View style={styles.goalsContainer}>
-        <Text>List of Goals</Text>
+        {courseGoals.map((goal) => (
+          <View style={styles.goalItem} key={goal}>
+            <Text style={styles.goalText}>{goal}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({  
-  appContainer:{
-    flex:1,
-    padding:50
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    padding: 50,
   },
-  inputContainer:{
+  inputContainer: {
     // backgroundColor:"red",
-    flex:1,
-    flexDirection:"row",
-    justifyContent:"space-between",
-    alignItems:"center",
-    marginBottom:24,
-    borderBottomWidth:1,
-    borderBottomColor:"#cccccc"
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
-  goalsContainer:{
-    flex:5
+  goalsContainer: {
+    flex: 5,
   },
-  textInput:{
-    borderWidth:1,
-    borderColor:"#cccccc",
-    width:"70%",
-    marginRight:8,
-    padding:8
-  }
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+    color: "white",
+  },
+  goalText: {
+    color: "white",
+  },
 });
